f1bf76e10b2919a4c396945b185d5248148aac91
Inbeom Hwang <hwanginbeom@gmail.com>
2014-02-22 02:03:18 +0900

    Add customization options for serverengine

--- lib/hightops.rb
+++ lib/hightops.rb
@@ -20,12 +20,19 @@ module Hightops
       if block_given?
         yield config
 
+        serverengine_config = {
+          supervisor: true,
+          daemon_process_name: "hightops-supervisor-#{config.project_name}-#{config.service_name}",
+          worker_process_name: "hightops-worker-#{config.project_name}-#{config.service_name}",
+          server_process_name: "hightops-server-#{config.project_name}-#{config.service_name}"
+        }
+
         # We ignore env configuration for Sneakers as it is too tied to its
         # internals.
         #
         # Note: Sneakers 0.1.0 has an additional configuration option to ignore
         # environment variable with Sneakers.not_environmental!
-        Sneakers.configure env: nil, amqp: config.amqp
+        Sneakers.configure serverengine_config.merge(env: nil, log: config.log, amqp: config.amqp)
       end
     end
   end
