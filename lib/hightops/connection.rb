25f6b4af94c39f4ddedaf923b89129b3056ef8ab
Inbeom Hwang <hwanginbeom@gmail.com>
2014-03-19 18:43:32 +0900

    Reset connection when needed to prevent connection closed error

--- lib/hightops/connection.rb
+++ lib/hightops/connection.rb
@@ -10,12 +10,15 @@ module Hightops
       @amqp = Sneakers::Config[:amqp]
       @vhost = Sneakers::Config[:vhost]
       @heartbeat = Sneakers::Config[:heartbeat]
+      @mutex = Mutex.new
     end
 
     # Public: Channel for the connection.
     #
     # Returns new channel associated to the connection.
     def channel
+      reset_connection unless connected?
+
       @channel ||= bunny_connection.create_channel
     end
 
@@ -25,7 +28,24 @@ module Hightops
     #
     # Returns bunny connection.
     def bunny_connection
-      @bunny_connection ||= Bunny.new(@amqp, vhost: @vhost, heartbeat: @heartbeat).tap { |connection| connection.start }
+      @mutex.synchronize do
+        @bunny_connection ||= Bunny.new(@amqp, vhost: @vhost, heartbeat: @heartbeat).tap { |connection| connection.start }
+      end
+    end
+
+    # Internal: Checks connection status of bunny_connection.
+    #
+    # Returns true if bunny_connection is alive, false otherwise.
+    def connected?
+      bunny_connection.connected?
+    end
+
+    # Internal: Resets connection handles initialized before.
+    #
+    # Returns nil.
+    def reset_connection
+      @bunny_connection = nil
+      @channel = nil
     end
   end
 end
